513365f586739e1585b869fe31659acf63dfd7ae
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2023-03-13 11:44:23 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid} from '@giphy/react-components'
+import { GiphyFetch } from '@giphy/js-fetch-api'
+import { useState } from 'react';
+
+
 
 function App() {
+  const [query, setQuery] = useState<string>('');
+  const [search, setSearch] = useState<string>('dogs');
+  // use @giphy/js-fetch-api to fetch gifs, instantiate with your api key
+  const gf = new GiphyFetch('f31eKY8GpuCfx8Az5xs8cQ4irhnhVrm9')
+
+  const onChange: (event: React.ChangeEvent<HTMLInputElement>) => void = (event) => {
+    setQuery(event.target.value);
+  }
+
+  const fetchGifs = (offset: number) => gf.search(search, { offset, limit: 10 })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Giphy Search</h3>
+        <div className='search'>
+          <input id="search" type="text" value={query} onChange={onChange}/>
+          <button onClick={() => setSearch(query)}>Search</button>
+        </div>
+        <Grid width={800} columns={3} gutter={6} fetchGifs={fetchGifs} key={search} />
       </header>
     </div>
   );
